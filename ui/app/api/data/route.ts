90cedb508d11b6cc6f799e1969f08b83f4a3cad8
Youtpout <EddyBoughioul@hotmail.fr>
2025-02-16 13:15:21 +0100

    remove handler

--- ui/app/api/data/route.ts
+++ ui/app/api/data/route.ts
@@ -14,31 +14,6 @@ type InputData = {
     time: number
 }
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<ResponseData>
-) {
-    console.log("request", req.method);
-    if (req.method === 'POST') {
-        const json = req.body as InputData;
-        console.log(json)
-
-        const supabase = createClient('https://heinegxlzmatuyvslqnz.supabase.co', process.env.key);
-        const { error } = await supabase
-            .from('benchmark')
-            .insert(json);
-
-        if (error) {
-            return res.status(500).json({ message: error.details });
-        }
-        return res.status(200).json({ message: 'saved' });
-
-    } else {
-        return res.status(404);
-    }
-}
-
-
 export async function POST(
     req: Request
 ) {
