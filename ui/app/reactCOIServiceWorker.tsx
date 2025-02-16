4cfe844eef58ede716e5661d3e8889a6f14ef3bf
Youtpout <EddyBoughioul@hotmail.fr>
2025-02-16 10:33:12 +0100

    correct react coi folder

--- ui/app/reactCOIServiceWorker.tsx
+++ ui/app/reactCOIServiceWorker.tsx
@@ -6,7 +6,7 @@ function loadCOIServiceWorker() {
     window.location.hostname != 'localhost'
   ) {
     const coi = window.document.createElement('script');
-    coi.setAttribute('src', '/proof-benchmark/coi-serviceworker.min.js'); // update if your repo name changes for 'npm run deploy' to work correctly
+    coi.setAttribute('src', '/coi-serviceworker.min.js'); // update if your repo name changes for 'npm run deploy' to work correctly
     window.document.head.appendChild(coi);
   }
 }
